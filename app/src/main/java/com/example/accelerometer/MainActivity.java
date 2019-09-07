66124e3554f59c3c9a4e33409dfa370f2e05f2dc
newmi596 <newmi596@student.otago.ac.nz>
2019-09-07 14:47:15 +1200

    commit

--- app/src/main/java/com/example/accelerometer/MainActivity.java
+++ app/src/main/java/com/example/accelerometer/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.accelerometer;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.solver.widgets.Helper;
-
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Color;
 import android.hardware.Sensor;
@@ -18,16 +15,17 @@ import android.util.Log;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Timer;
 import java.util.TimerTask;
-
-import javax.xml.transform.dom.DOMLocator;
 
 import static java.lang.Math.sqrt;
 
@@ -37,17 +35,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Sensor accelerometer;
     public Vibrator v;
     private float vibrateThreshold = 0;
-    public double X, Y, Z, magnitude, transformedmag,fftave;
-    public ArrayList<Double> transformedX, transformedY, transformedZ;
-    public int p, period, i, s;
-    private MediaPlayer slow,med,fast;
+    public double X, Y, Z, magnitude, fftave;
+    public int p, period, i, s, windsize ;
+    private MediaPlayer slow, med, fast;
     public SeekBar sampleRate, windowsize;
-    public GraphView graph,FFTGraph;
-    public Timer timer = new Timer(),ffttimer = new Timer();
+    public GraphView graph, FFTGraph;
+    public Timer timer = new Timer(), ffttimer = new Timer();
     public FFT fft;
-    public LineGraphSeries < DataPoint > seriesx, seriesy, seriesz, seriesmagnitude, fftmagnitude,fftseries;
+    public Queue<Double> magQueue = new LinkedList<>();
+    public LineGraphSeries<DataPoint> seriesx, seriesy, seriesz, seriesmagnitude, fftseries;
     public TextView state;
-
+    public double[] fftmag,q, gravity = new double[3];
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -55,29 +53,22 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initializeViews();
-
-
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        assert sensorManager != null;
         if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
             // success! we have an accelerometer
 
             accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
             sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
             vibrateThreshold = accelerometer.getMaximumRange() / 2;
-        } else {
-            // fail! we dont have an accelerometer!
         }
-        //initialize vibration
         v = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
 
         timer.schedule(new TimerTask() {
-
             @Override
             public void run() {
                 appendToSeries();
-                Log.e("STATE", "FFT VAL: " + fftave);
             }
-
         }, 0, 1000);
 
         ffttimer.schedule(new TimerTask() {
@@ -85,18 +76,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             public void run() {
                 updateSeries();
             }
-        }, 5001, 5001);
+        }, windsize*1000+1, windsize*1000+1);
     }
 
     protected void onResume() {
         super.onResume();
-        sensorManager.registerListener((SensorEventListener) this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     //onPause() unregister the accelerometer for stop listening the events
     protected void onPause() {
         super.onPause();
-        sensorManager.unregisterListener((SensorEventListener) this);
+        sensorManager.unregisterListener(this);
     }
 
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
@@ -105,10 +96,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     public void onSensorChanged(SensorEvent event) {
 
-        float deltaX = event.values[0];
-        float deltaY = event.values[1];
-        float deltaZ = event.values[2];
+        final float alpha = (float) 0.8;
 
+        // Isolate the force of gravity with the low-pass filter.
+        gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
+        gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
+        gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
+
+        // Remove the gravity contribution with the high-pass filter.
+        float deltaX = (float) (event.values[0] - gravity[0]);
+        float deltaY = (float) (event.values[1] - gravity[1]);
+        float deltaZ = (float) (event.values[2] - gravity[2]);
 
         // if the change is below 2, it is just plain noise
         if (deltaX < 2)
@@ -123,25 +121,112 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         Y = deltaY;
         Z = deltaZ;
 
-        if(!slow.isPlaying() && !med.isPlaying() && !fast.isPlaying()){
-            changePlayer(fftave);
-        }
-
-        if((slow.isPlaying() || med.isPlaying() || fast.isPlaying()) && fftave < 0.5 && deltaX==0 && deltaY==0) {
-            slow.stop();
-            med.stop();
-            fast.stop();
-            state.setText("Player Paused");
-        }
-
-        if    ((slow.isPlaying() && fftave>2)
-            || (med.isPlaying() && (fftave<=5 || fftave >7))
-            || (fast.isPlaying() && fftave <= 7))
-        {
-            changePlayer(fftave);
-        }
     }
 
+   public void changePlayer(final double fftave) {
+       runOnUiThread(new Runnable() {
+           @SuppressLint("SetTextI18n")
+           @Override
+           public void run() {
+              if(slow.isPlaying()) {
+                  slow.pause();
+              }else if(med.isPlaying()) {
+                  med.pause();
+              }else if(fast.isPlaying()) {
+                  fast.pause();
+              }
+              if (fftave <= 2 && fftave >= 0.5) {
+                  slow.start();
+                  state.setText("Slow playing");
+              } else if (fftave > 2 && fftave <= 5) {
+                  med.start();
+                  state.setText("Med playing");
+              } else if (fftave > 5) {
+                  fast.start();
+                  state.setText("Fast playing");
+              }
+           }
+       });
+   }
+
+    @SuppressLint("SetTextI18n")
+    public void updateSeries() {
+
+        FFTGraph.removeAllSeries();
+
+        double[] zeroes = new double[windsize];
+        q = new double[windsize];
+
+        for(int p = 0; p < q.length; p++){
+            q[p] = magQueue.remove();
+
+        }
+        fft.fft(q, zeroes);
+
+        double max = 0;
+
+        for(int m = 0; m < q.length;m++) {
+            if(max<q[m]){
+               max=q[m];
+            }
+        }
+
+        fftave=max;
+
+        Log.e("STATE", "Average: " + fftave);
+
+        fftseries= new LineGraphSeries<>(data());
+        FFTGraph.addSeries(fftseries);
+
+        if (!slow.isPlaying() && !med.isPlaying() && !fast.isPlaying()) {
+            changePlayer(fftave);
+        }
+        runOnUiThread(new Runnable() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void run() {
+                if (fftave < 0.5 && X == 0 && Y == 0) {
+                    if(slow.isPlaying()){
+                        slow.pause();
+                    }else if(med.isPlaying()){
+                        med.pause();
+                    }else if(fast.isPlaying()){
+                        fast.pause();
+                    }
+                    state.setText("Player Paused");
+                }
+            }
+        });
+
+        if ((slow.isPlaying() && fftave > 2)
+                || (med.isPlaying() && (fftave <= 5 || fftave > 7))
+                || (fast.isPlaying() && fftave <= 7)) {
+            changePlayer(fftave);
+        }
+
+    }
+
+    public void appendToSeries() {
+        magnitude = sqrt(X * X + Y * Y + Z * Z);
+
+        //noinspection SuspiciousNameCombination
+        seriesx.appendData(new DataPoint(i, X), true, 10);
+        seriesy.appendData(new DataPoint(i, Y), true, 10);
+        seriesz.appendData(new DataPoint(i, Z), true, 10);
+        seriesmagnitude.appendData(new DataPoint(i, magnitude), true, 10);
+        i++;
+        magQueue.add(magnitude);
+    }
+
+    public DataPoint[] data(){
+        int n=windsize;     //to find out the no. of data-points
+        DataPoint[] values = new DataPoint[n];     //creating an object of type DataPoint[] of size 'n'
+        for(int i=0;i<n;i++){
+            DataPoint v = new DataPoint(i,q[i]);
+            values[i] = v;
+        }
+        return values;
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public void initializeViews() {
@@ -149,22 +234,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         med = MediaPlayer.create(this, R.raw.sound2);
         fast = MediaPlayer.create(this, R.raw.sound3);
 
+        windsize=4;
         state = findViewById(R.id.state);
-        fft = new FFT(2);
+        fft = new FFT(windsize);
         X = 0;
         Y = 0;
         Z = 0;
         magnitude = 0;
-        transformedmag = 0;
         fftave = 0.0;
-        transformedX = new ArrayList<>();
-        transformedY = new ArrayList<>();
-        transformedZ = new ArrayList<>();
+        fftmag = new double[windsize];
         period = 1000;
         graph = findViewById(R.id.graph);
         FFTGraph = findViewById(R.id.fftgraph);
         windowsize = findViewById(R.id.seekBar);
-
         sampleRate = findViewById(R.id.seekBarSampleRate);
 
         seriesx = new LineGraphSeries<>(new DataPoint[]{
@@ -179,16 +261,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         seriesmagnitude = new LineGraphSeries<>(new DataPoint[]{
                 new DataPoint(0, 0)
         });
-        fftmagnitude = new LineGraphSeries<>(new DataPoint[]{
-                new DataPoint(0, 0)
-        });
-        fftseries = new LineGraphSeries<>(new DataPoint[]{
+        fftseries= new LineGraphSeries<>(new DataPoint[]{
                 new DataPoint(0, 0)
         });
 
         FFTGraph.getViewport().setXAxisBoundsManual(true);
         FFTGraph.getViewport().setMinX(0);
-        FFTGraph.getViewport().setMaxX(5);
+        FFTGraph.getViewport().setMaxX(windsize);
 
         graph.getViewport().setXAxisBoundsManual(true);
         graph.getViewport().setMinX(0);
@@ -208,13 +287,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sampleRate.setMin(100);
         sampleRate.setMax(5000);
         sampleRate.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-             @Override
+            @Override
             public void onProgressChanged(SeekBar seekBar, int period, boolean b) {
                 p = period;
 
-                transformedX.clear();
-                transformedY.clear();
-                transformedZ.clear();
             }
 
             @Override
@@ -241,7 +317,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                         FFTGraph.addSeries(fftseries);
 
                     }
-                }, 5 * p, 5 * p);
+                }, windsize*p+1, windsize*p+1);
             }
         });
 
@@ -253,107 +329,22 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 s = i;
             }
-
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
             }
-
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                int size = (int)Math.pow(2, s);
-                FFTGraph.getViewport().setMaxY(s+10);
-                fft = new FFT(size);
+                windsize = (int) Math.pow(2, s);
+                fft = new FFT(windsize);
+                ffttimer.cancel();
+                ffttimer = new Timer();
+                ffttimer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        updateSeries();
+                    }
+                }, windsize*1000+1, windsize*1000+1);
             }
         });
     }
-
-
-
-    public double[] getTransformed(ArrayList<Double> list){
-
-        double x1 = list.get(0);
-        double x2 = list.get(1);
-        double x3 = list.get(2);
-        double x4 = list.get(3);
-        double x5 = list.get(4);
-
-        double[] fx = {x1, x2, x3, x4, x5};
-        return fx;
-    }
-
-    public double[] getMagnitudeArray(double[] fx, double[] fy, double[] fz){
-        double[] a = new double[5];
-        for(int k=0;k<5;k++){
-            a[k] = sqrt(fx[k] * fx[k] + fy[k] * fy[k] + fz[k] * fz[k]);
-        }
-        return a;
-    }
-
-    public void changePlayer(double fftave){
-        slow.stop();
-        med.stop();
-        fast.stop();
-
-        if(fftave <= 2 && fftave >= 0.5){
-            slow.start();
-            state.setText("Slow is Playing");
-        }else if(fftave > 2 && fftave <= 5){
-            med.start();
-            state.setText("Medium is Playing");
-        }else if(fftave > 5){
-            fast.start();
-            state.setText("Fast is Playing");
-        }
-    }
-
-    public void updateSeries(){
-
-
-        FFTGraph.removeAllSeries();
-
-        double[] zeroes = { 0, 0, 0, 0, 0 };
-
-        double[] fx = getTransformed(transformedX);
-        double[] fy = getTransformed(transformedY);
-        double[] fz = getTransformed(transformedZ);
-
-        fft.fft(fx, zeroes);
-        fft.fft(fy, zeroes);
-        fft.fft(fz, zeroes);
-
-        double[] magnitudes = getMagnitudeArray(fx,fy,fz);
-
-        fftave = (magnitudes[0] + magnitudes[1] + magnitudes[2] + magnitudes[3] + magnitudes[4])/5;
-
-        LineGraphSeries<DataPoint> fftseries = new LineGraphSeries<>(new DataPoint[]{
-                new DataPoint(0, magnitudes[0]),
-                new DataPoint(1, magnitudes[1]),
-                new DataPoint(2, magnitudes[2]),
-                new DataPoint(3, magnitudes[3]),
-                new DataPoint(4, magnitudes[4])
-        });
-
-        FFTGraph.addSeries(fftseries);
-
-        transformedX.clear();
-        transformedY.clear();
-        transformedZ.clear();
-
-    }
-
-    public void appendToSeries(){
-        magnitude = sqrt(X*X + Y*Y + Z*Z);
-
-        seriesx.appendData(new DataPoint(i,X),true,10);
-        seriesy.appendData(new DataPoint(i,Y),true,10);
-        seriesz.appendData(new DataPoint(i,Z),true,10);
-        seriesmagnitude.appendData(new DataPoint(i, magnitude), true, 10);
-        i++;
-        transformedX.add(X);
-        transformedY.add(Y);
-        transformedZ.add(Z);
-    }
-
 }
-
